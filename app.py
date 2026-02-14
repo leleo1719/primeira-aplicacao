5ae53319a80ae46a81447644b9f0849d2a3d8678
Leonardo Medeiros <leonardovm.dev@gmail.com>
2026-02-14 15:29:27 -0300

    configurando a lista de restaurantes

--- app.py
+++ app.py
@@ -38,8 +38,12 @@ def exibir_subtitulo(texto):
 def cadastrar_novo_restaurante():
     exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False}
+    restaurantes.append(dados_do_restaurante)
+
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
@@ -52,6 +56,23 @@ def listar_restaurantes():
         print(f'- {nome_restaurante} | {categoria} | {ativo}')
     voltar_ao_menu_principal()
 
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alternando estado os restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso!' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso!'
+            print(mensagem)
+            
+    if not restaurante_encontrado:
+        print(f'O restaurante não foi encontrado')
+
+    voltar_ao_menu_principal()
+
 def escolher_opcoes():
     try:        
         opcao_escolhida = int(input('Escolha uma opção: '))
@@ -62,7 +83,7 @@ def escolher_opcoes():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Ativar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app() 
         else:
