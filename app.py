d82241556d45cc910ef6f838e038db329e74dbf6
Leonardo Medeiros <leonardovm.dev@gmail.com>
2026-02-14 09:23:20 -0300

    adicionado código

---
+++ app.py
@@ -0,0 +1,55 @@
+import os
+
+def exibir_nome_do_programa():
+    print("""
+━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
+┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱┃╭━━╯
+┃╰━━┳━━┫╰━┳━━┳━╮┃╰━━┳╮╭┳━━┳━┳━━┳━━┳━━╮
+╰━━╮┃╭╮┃╭╮┃╭╮┃╭╯┃╭━━┻╋╋┫╭╮┃╭┫┃━┫━━┫━━┫
+┃╰━╯┃╭╮┃╰╯┃╰╯┃┃╱┃╰━━┳╋╋┫╰╯┃┃┃┃━╋━━┣━━┃
+╰━━━┻╯╰┻━━┻━━┻╯╱╰━━━┻╯╰┫╭━┻╯╰━━┻━━┻━━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯""") 
+def exibir_opcoes():
+    
+    print('1. Cadastrar restaurante') 
+    print('2. Listar restaurante') 
+    print('3. Ativar restaurante') 
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('clear')
+    print(f'Finalizando app...\n')
+
+def opcao_invalida():
+    print(f'Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal: ')
+    main()
+
+def escolher_opcoes():
+    try:        
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # print(f'Você escolheu a opção: {opcao_escolhida}')
+
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurante')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app() 
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida() 
+
+def main():
+    os.system('clear')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__=='__main__':
+    main()
+
